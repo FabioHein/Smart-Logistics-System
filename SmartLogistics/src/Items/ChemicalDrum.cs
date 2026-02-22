5d9c6b86a9c4f0d457de4f16284221ecef53131d
FabioHein <fabio.heinreichsberger@gmail.com>
2026-02-22 20:08:47 +0100

    Klasse ChemicalDrum erstellt.

---
+++ SmartLogistics/src/Items/ChemicalDrum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartLogistics.src.Enums;
+using SmartLogistics.src.Interfaces;
+using SmartLogistics.src.Items;
+
+namespace SmartLogistics.src.Items
+{
+    public class ChemicalDrum : Freight, IRefrigerated
+    {
+
+        public ChemicalDrum(string id, double weight) : base(id , weight)
+        {
+        }
+
+        public void CheckTemperature()
+        {
+            Console.WriteLine($"Die Temperatur wurde geprüft.");
+        }
+    }
+}
