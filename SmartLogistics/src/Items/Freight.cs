b6f7ac9304c4eeb8dff7cd2536e8c2606305f4bf
FabioHein <fabio.heinreichsberger@gmail.com>
2026-02-22 19:56:06 +0100

    Basisklasse mit Konstruktor erstellt.

--- SmartLogistics/src/Items/Freight.cs
+++ SmartLogistics/src/Items/Freight.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace SmartLogistics.src.Items
 {
-    internal class Freight
+    public abstract class Freight
     {
+        public double _weight { get; protected set; }
+        public string _id { get; protected set; }
+
+        public Freight(string id, double weight)
+        {
+            _id = id;
+            _weight = weight;
+        }
     }
 }
