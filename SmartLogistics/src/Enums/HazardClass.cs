3a841c076594461c5619557cec3af3b33bd8d892
FabioHein <fabio.heinreichsberger@gmail.com>
2026-02-22 19:46:21 +0100

    Ordner Struktur und Basisklasse erstellt

---
+++ SmartLogistics/src/Enums/HazardClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartLogistics.src.Enums
+{
+    internal class HazardClass
+    {
+    }
+}
---
+++ SmartLogistics/src/Items/Freight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartLogistics.src.Items
+{
+    internal class Freight
+    {
+    }
+}
