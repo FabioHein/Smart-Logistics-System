49f80c74395d55ffd58a5d28f38a8cd8da013c5a
FabioHein <fabio.heinreichsberger@gmail.com>
2026-02-24 20:03:45 +0100

    Klasse SteelBeam wurde hinzugefügt.

---
+++ SmartLogistics/src/Items/SteelBeam.cs
@@ -0,0 +1,20 @@
+﻿using SmartLogistics.src.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartLogistics.src.Items
+{
+    public class SteelBeam : Freight, IHeavyLoad
+    {
+        public SteelBeam(string id, double weight) : base( id, weight)
+        {
+        }
+
+        
+        public void SecureWithChains()
+        {
+            Console.WriteLine($"Der Stahlträger {Id}, wird mit Ketten gesichert.");
+        }
+    }
+}
