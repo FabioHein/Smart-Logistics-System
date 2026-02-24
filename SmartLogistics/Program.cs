17c18d41aac50643e376917d462fd88cdf7135e8
FabioHein <fabio.heinreichsberger@gmail.com>
2026-02-24 20:23:56 +0100

    Liste der Fracht erstellt und Schleife für Methoden aufruf hinzugefügt.

--- SmartLogistics/Program.cs
+++ SmartLogistics/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SmartLogistics.src.Items;
+using SmartLogistics.src.Enums;
+using System.Globalization;
+using SmartLogistics.src.Interfaces;
+
+List<Freight> fracht = new List<Freight>();
+
+fracht.Add(new SteelBeam("HEB 240", 800));
+fracht.Add(new ChemicalDrum("00-25", 250, HazardClass.Toxic));
+
+foreach(var palette in fracht)
+{
+    if (palette is IHeavyLoad e)
+    {
+        e.SecureWithChains();
+    }
+    if (palette is IRefrigerated a)
+    {
+        a.CheckTemperature();
+    }
+}
