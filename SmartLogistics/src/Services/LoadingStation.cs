c837d227e57398ff28773ae3889b4c05368a51b8
FabioHein <fabio.heinreichsberger@gmail.com>
2026-02-26 20:16:01 +0100

    Service LoadingStation hinzugefügt.

---
+++ SmartLogistics/src/Services/LoadingStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using SmartLogistics.src.Enums;
+using SmartLogistics.src.Interfaces;
+using SmartLogistics.src.Items;
+
+namespace SmartLogistics.src.Services
+{
+    public class LoadingStation
+    {
+         
+        public double LoadingMath(List<Freight> fracht)
+        {
+            double weight = 0;
+            foreach (var palette in fracht)
+            {
+                weight += palette.Weight;
+            }
+            return weight;
+        }
+        
+    }
+}
